cff25f4d79db8ca010ff34f96904e6642db01a42
ProkyIts <prokyits@gmail.com>
2024-12-05 15:46:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity if needed. Here's an example of a simple form submission alert.
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("This function is not working right now. Please come back later.");
+});
